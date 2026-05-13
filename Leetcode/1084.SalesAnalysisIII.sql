ab820a496ee036c144538d4664bc2235e6f959ee
Rajendra Patil <77819809+rajendra-patil96@users.noreply.github.com>
2026-05-13 19:11:22 +0530

    Update 1084.SalesAnalysisIII.sql

--- Leetcode/1084.SalesAnalysisIII.sql
+++ Leetcode/1084.SalesAnalysisIII.sql
@@ -7,5 +7,5 @@ SELECT
 FROM product p
 JOIN sales s
 ON p.product_id = s.product_id
-GROUP BY p.product_id,p.product_name
+GROUP BY p.product_id
 HAVING MIN(s.sale_date) >= "2019-01-01" AND MAX(s.sale_date) <= "2019-03-31";
