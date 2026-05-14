7fa486030202002bb5741ccfbc4a489c6561c3a8
Rajendra Patil <77819809+rajendra-patil96@users.noreply.github.com>
2026-05-14 12:56:42 +0530

    Create 1141.UserActivityforthePast30DaysI.sql

---
+++ Leetcode/1141.UserActivityforthePast30DaysI.sql
@@ -0,0 +1,9 @@
+--github.com/rajendra-patil96
+--SQL/leetcode/1141. User Activity for the Past 30 Days I
+
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE activity_date BETWEEN DATE_SUB("2019-07-27", INTERVAL 29 DAY) AND "2019-07-27"
+GROUP BY activity_date;
